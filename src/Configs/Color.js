d37d0f905e59f959880fe2657c679a29665475b9
Sutan Nasution <sutan.gnst@gmail.com>
2019-11-23 02:15:11 +0700

    remove unused color templates

--- src/Configs/Color.js
+++ src/Configs/Color.js
@@ -1,17 +1,7 @@
 export const Primary = '#e52f17'
 export const Secondary = '#bbb'
-export const Accent = '#ff89cb'
-export const Info = '#11cdef'
-export const Success = '#2dce89'
-export const Warning = '#fb6340'
-export const Danger = '#f5365c'
 
 export default {
     Primary,
-    Secondary,
-    Accent,
-    Info,
-    Success,
-    Warning,
-    Danger
+    Secondary
 }
